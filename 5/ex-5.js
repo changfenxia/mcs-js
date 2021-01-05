a0bddda4624c14f4e1442d97ea9583aaa695fd5f
Artem Arbatskiy <artyom.arbatskiy@gmail.com>
2021-01-05 19:03:59 +0800

    rock scissors paper

---
+++ 5/ex-5.js
@@ -0,0 +1,32 @@
+const rock = 0;
+const scissors = 1;
+const paper = 2;
+
+// для вывода результатов броска в консоль
+const check = ['rock', 'scissors', 'paper'];
+
+// в этот элемент выведем результат
+const resultElement = document.querySelector('.result');
+
+// попросим игрока ввести ответ в виде числа
+let player = parseInt(prompt('Your choice: 0 - rock, 1 - scissors, 2 - paper'));
+// Рандомное число 0, 1 или 2 — разберем это позже, пока не заморачивайтесь
+let computer = Math.floor(Math.random() * 3);
+
+// опишем все условия и будем выводить в консоль 'computer win' или 'player win'
+let result = 'Draw!'
+
+if ((player == 0 && computer == 1)
+|| (player == 1 && computer == 2)
+|| (player == 2 && computer == 0)) {
+    result = 'Player win!';
+} else if ((computer == 0 && player == 1)
+|| (computer == 1 && player == 2)
+|| (computer == 2 && player == 0)) {
+    result = 'Computer win!';
+}
+
+console.log('computer: ', check[computer]);
+console.log('player: ', check[player]);
+console.log(result);
+resultElement.textContent = result;
