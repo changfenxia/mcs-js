df330ae4cefaa05227fb71ed8c5b145f01522061
Artem Arbatskiy <artyom.arbatskiy@gmail.com>
2021-01-05 19:58:37 +0800

    add task text

--- 6/script.js
+++ 6/script.js
@@ -1,8 +1,6 @@
-// var totalCash = parseInt(prompt("Сколько у тебя кэша?"));
-// var watchesCount = parseInt(prompt("Сколько часов будем брать?"));
-// var earringsCount = parseInt(prompt("Сколько сережек будем брать?"));
-// var watchesPrice = parseInt(prompt("Сколько стоят часы?"));
-// var earringsPrice = parseInt(prompt("Сколько стоят сережки?"));
+// Новости от людей Кевина Гарнетта. «Кей Джи» надоело каждый раз перезагружать страницу с приложением для подсчета стоимости выбранных драгоценностей. Он просит переписать всю программу в виде функции haveEnough, которую можно будет вызывать хоть 10 раз подряд.
+
+// Не уроните доверие легенды NBA — распишите параметры и код в теле функции таким образом, чтобы в качестве аргументов использовались все те же переменные, что и раньше, а функция возвращала «Enough $$$» или «Not enough $$$» в зависимости от введенных значений.
 
 const haveEnough = (totalCash, watchesCount, earringsCount, watchesPrice, earringsPrice) => {
         const watchesSum = watchesCount * watchesPrice;
