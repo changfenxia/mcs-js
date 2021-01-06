95e906f0d7d70ec889d4e226e5611c63cf1cbd95
Artem Arbatskiy <artyom.arbatskiy@gmail.com>
2021-01-07 00:06:57 +0800

    ex 8

---
+++ 8/js/script.js
@@ -0,0 +1,12 @@
+// Создайте форму, которая будет по сабмиту рассчитывать синус введенного в input угла. Вам понадобится самостоятельно найти этот метод объекта Math.
+
+window.onload = function() {
+    const angle = document.forms[0].elements.angle;
+    const btn = document.querySelector('.submit');
+    const result = document.querySelector('.result');
+
+    btn.addEventListener('click', function(e) {
+        e.preventDefault();
+        result.textContent = Math.sin(angle.value);
+    });
+}
