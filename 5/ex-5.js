a229b6b26dbccfe46f5cf28bf278731c66e539d9
Artem Arbatskiy <artyom.arbatskiy@gmail.com>
2021-01-06 21:55:52 +0800

    == to ===

--- 5/ex-5.js
+++ 5/ex-5.js
@@ -14,15 +14,15 @@ let player = parseInt(prompt('Your choice: 0 - rock, 1 - scissors, 2 - paper'));
 let computer = Math.floor(Math.random() * 3);
 
 // опишем все условия и будем выводить в консоль 'computer win' или 'player win'
-let result = 'Draw!'
+let result;
 
-if ((player == 0 && computer == 1)
-|| (player == 1 && computer == 2)
-|| (player == 2 && computer == 0)) {
+if (player === computer) {
+    result = 'Draw!';
+} else if ((player === 0 && computer === 1)
+|| (player === 1 && computer === 2)
+|| (player === 2 && computer === 0)) {
     result = 'Player win!';
-} else if ((computer == 0 && player == 1)
-|| (computer == 1 && player == 2)
-|| (computer == 2 && player == 0)) {
+} else {
     result = 'Computer win!';
 }
 
